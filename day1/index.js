313eb1632ce44e6c69e1cbdf60bd4bd38c537cc5
Christian Maulany <cmaulany@gmail.com>
2022-12-01 09:33:35 +0100

    Day 1

--- day1/index.js
+++ day1/index.js
@@ -1,3 +1,17 @@
-export default function day1 () {
-    console.log("Hello World");
+import { readFileSync } from 'fs';
+
+export default function day1() {
+    const input = readFileSync('./day1/input.txt', { encoding: 'utf8' });
+    
+    const elves = input.split('\n\n').map((elfInput) => elfInput.split('\n').map(Number));
+    
+    const sum = (numbers) => numbers.reduce((sum, n) => sum + n, 0);
+    
+    const elfSums = elves.map(sum).sort((a, b) => a < b ? 1 : -1);
+    
+    const maxElfSum = elfSums[0];
+    const topThreeElvesSum = sum(elfSums.slice(0 ,3));
+
+    console.log(`Answer part 1: ${maxElfSum}`);
+    console.log(`Answer part 2: ${topThreeElvesSum}`);
 }
