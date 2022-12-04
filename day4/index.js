b98fabb85723f73eb372bc2b3ed00d098846bbb7
Christian Maulany <cmaulany@gmail.com>
2022-12-04 11:24:40 +0100

    Day 4

---
+++ day4/index.js
@@ -0,0 +1,35 @@
+import { readFileSync } from 'fs';
+
+const fullyContains = (a, b) => (
+    (a.start <= b.start && a.end >= b.end) ||
+    (b.start <= a.start && b.end >= a.end)
+);
+
+const overlaps = (a, b) => (
+    (a.start <= b.start && a.end >= b.start) ||
+    (b.start <= a.start && b.end >= a.start)
+);
+
+export default function day4() {
+    const input = readFileSync('./day4/input.txt', { encoding: 'utf8' });
+
+    const pairs = input.split('\n').map((line) => {
+        const [startA, endA, startB, endB] = line.match(/^(\d+)-(\d+),(\d+)-(\d+)$/).slice(1).map(Number);
+        return [
+            {
+                start: startA,
+                end: endA,
+            },
+            {
+                start: startB,
+                end: endB
+            }
+        ];
+    });
+
+    const fullyContainedPairs = pairs.filter(pair => fullyContains(...pair));
+    const overlappingPairs = pairs.filter(pair => overlaps(...pair));
+
+    console.log(`Answer part 1: ${fullyContainedPairs.length}`);
+    console.log(`Answer part 2: ${overlappingPairs.length}`);
+}
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import day1 from './day1/index.js';
 import day2 from './day2/index.js';
 import day3 from './day3/index.js';
+import day4 from './day4/index.js';
 
 const dayArg = process.argv[2];
 if (dayArg === undefined) {
@@ -11,6 +12,7 @@ const day = {
     '1': day1,
     '2': day2,
     '3': day3,
+    '4': day4,
 }[dayArg];
 
 if (!day) {
