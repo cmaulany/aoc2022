8e03595128c3f680b0d1a1063512f3a08aade803
Christian Maulany <cmaulany@gmail.com>
2022-12-05 08:59:52 +0100

    Day 5 with harcoded stackCount and maxStackHeight

--- day5/index.js
+++ day5/index.js
@@ -1,11 +1,29 @@
 import { readFileSync } from 'fs';
 
+function copyStacks(stacks) {
+    return stacks.map((stack) => stack.slice());
+}
+
+function tick(stacks, move, flipOrder = false) {
+    stacks = copyStacks(stacks);
+
+    const temp = [];
+    for (let i = 0; i < move.count; i++) {
+        const crate = stacks[move.from - 1].pop();
+        temp.push(crate);
+    }
+    const flipped = flipOrder ? temp.reverse() : temp;
+    stacks[move.to - 1].push(...flipped);
+
+    return stacks;
+}
+
 export default function day5() {
     const input = readFileSync('./day5/input.txt', { encoding: 'utf8' });
     const lines = input.split('\n');
 
-    const stackCount = 3;
-    const maxStackHeight = 3;
+    const stackCount = 9;
+    const maxStackHeight = 8;
     const stacks = [];
     for (let i = 0; i < stackCount; i++) {
         stacks[i] = [];
@@ -18,7 +36,7 @@ export default function day5() {
     }
 
     const moves = lines.slice(maxStackHeight + 2).map((line) => {
-        const [count, from, to] = line.match(/move (\d) from (\d) to (\d)/).slice(1).map(Number);
+        const [count, from, to] = line.match(/move (\d+) from (\d) to (\d)/).slice(1).map(Number);
         return {
             count,
             from,
@@ -26,7 +44,12 @@ export default function day5() {
         };
     });
 
+    const finalState9000 = moves.reduce((stacks, move) => tick(stacks, move, false), stacks);
+    const finalState9001 = moves.reduce((stacks, move) => tick(stacks, move, true), stacks);
 
-    console.log(moves);
+    const result9000 = finalState9000.map((stack) => stack.pop()).join('');
+    const result9001 = finalState9001.map((stack) => stack.pop()).join('');
 
+    console.log(`Answer part 1: ${result9000}`);
+    console.log(`Answer part 2: ${result9001}`);
 }
