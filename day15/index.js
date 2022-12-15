5f73e9a01298b0335aa8e2cb7d29acda61578f95
cmaulany <christian.maulany@kpn.com>
2022-12-15 13:28:18 +0100

    Answer day 15, messy code

--- day15/index.js
+++ day15/index.js
@@ -1,27 +1,6 @@
 import { readFileSync } from 'fs';
 
-// const toKey = (position) => `${position.x},${position.y}`;
-
-// function addReport(map, report) {
-//     const { sensor, beacon } = report;
-//     const dx = Math.abs(beacon.x - sensor.x);
-//     const dy = Math.abs(beacon.y - sensor.y);
-
-//     const distance = dx + dy;
-
-//     map[toKey(sensor)] = 'S';
-//     map[toKey(beacon)] = 'B';
-
-//     for (let x = sensor.x - dx; x <= sensor.x + dx; x++) {
-//         for (let y = sensor.y - dy; y <= sensor.y + dy; y++) {
-//             if (Math.abs(x - sensor.x) + Math.abs(y - sensor.y) <= distance) {
-//                 map[toKey({ x, y })] ??= '#';
-//             }
-//         }
-//     }
-
-//     return map;
-// }
+const cap = 4000000;
 
 export default function day15() {
     const input = readFileSync('./day15/input.txt', { encoding: 'utf8' });
@@ -50,43 +29,110 @@ export default function day15() {
 
     });
 
-    const countRow = (reports, rowIndex) => {
-        const xs = new Set();
+    function mergeRanges(ranges) {
+        if (ranges.length === 1) {
+            return ranges;
+        }
+
+        const merged = ranges.sort((a, b) => a.start - b.start);
+        for (let i = 1; i < merged.length; i++) {
+            const current = merged[i];
+            const previous = merged[i - 1];
+            if (current && current.start >= previous.start && current.start <= previous.end) {
+                merged.splice(
+                    i - 1,
+                    2,
+                    {
+                        start: previous.start,
+                        end: Math.max(current.end, previous.end)
+                    }
+                );
+                i--;
+            }
+        }
+        return merged;
+    }
+
+    function clampRanges(ranges, min, max) {
+        const newRanges = ranges.slice();
+        for (let i = 0; i < newRanges.length; i++) {
+            const range = newRanges[i];
+            if (range.end < min || range.start > max) {
+                newRanges.splice(i, 1);
+                i--;
+            }
+            else {
+                newRanges[i] = {
+                    start: Math.max(min, range.start),
+                    end: Math.min(max, range.end),
+                };
+            }
+        }
+        return newRanges;
+    }
+
+    function count(ranges) {
+        return ranges.reduce((sum, { start, end }) => sum + end + 1 - start, 0);
+    }
+
+    const countRow = (reports, rowIndex, clamp) => {
+        const ranges = [];
         reports.forEach((report) => {
             const { sensor } = report;
             const dy = Math.abs(rowIndex - sensor.y);
             const overlap = sensor.range - dy;
             if (overlap >= 0) {
-                for (let x = -overlap; x <= overlap; x++) {
-                    xs.add(sensor.x + x);
-                }
+                ranges.push({
+                    start: sensor.x - overlap,
+                    end: sensor.x + overlap
+                });
             }
         });
 
-        const otherInRow = new Set(
-            [
-                ...reports
-                    .map(({ beacon }) => beacon)
-                    .filter((beacon) => beacon.y === rowIndex)
-                    .map((beacon) => beacon.x),
-                ...reports
-                    .map(({ sensor }) => sensor)
-                    .filter((sensor) => sensor.y === rowIndex)
-                    .map((sensor) => sensor.x)
-            ]
-        );
+        const merged = mergeRanges(ranges);
+        const clamped = clamp !== undefined ? clampRanges(merged, 0, clamp) : merged;
 
-        // console.log(xs, beaconsInRow);
+        // const junk = new Set([
+        //     ...reports
+        //         .map(({ beacon }) => beacon)
+        //         .filter((beacon) => beacon.y === rowIndex)
+        //         .map((beacon) => beacon.x),
+        //     ...reports
+        //         .map(({ sensor }) => sensor)
+        //         .filter((sensor) => sensor.y === rowIndex)
+        //         .map((sensor) => sensor.x)
+        // ]);
 
-        return xs.size - otherInRow.size;
+        return clamped; // - junk.size;
     };
 
     // const map = reports.reduce(addReport, {});
     // console.log(countRow(reports, 9));
 
-    for (let i = 0 )
-
     // console.log(countRow(reports, 2000000));
+    for (let y = 0; y <= cap; y++) {
+        // if (y % 100000 === 0) {
+        //     console.log(y);
+        // }
+        const clamped = countRow(reports, y, cap);
+        if (count(clamped) <= cap) {
+            console.log("Y", y, clamped);
+        }
+        // console.log(test);
+    }
+    const coord = [3120101, 2634249];
+    // console.log('done');
+    // console.log(mergeRanges([{ start: 10, end: 15 }, { start: 12, end: 20 }]));
+    // console.log(mergeRanges([{ start: 10, end: 30 }, { start: 12, end: 20 }]));
+    // console.log(mergeRanges([{ start: 10, end: 15 }, { start: 18, end: 20 }]));
+    // const clamped = clampRanges(
+    //     [
+    //         { start: -10, end: 20 },
+    //         { start: -5, end: -8 },
+    //         { start: -10, end: 60 },
+    //         { start: 30, end: 60 }
+    //     ], 0, 40);
+    // console.log(clamped);
 
     // console.log(reports);
 }
